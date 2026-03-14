8707bcc98f20443eb2e2678af0955e1edfdff419
Hachicha Hassan <hsan.isetsf@gmail.com>
2026-03-14 12:28:41 +0100

    client indexversion F

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv').config();
+const path = require('path'); // Ajout de l'importation de path
 const app = express();
 // Middleware pour parser le JSON
 app.use(express.json());
@@ -18,7 +19,9 @@ app.use('/api/categories', require('./routes/categorie.route'));
 app.use("/api/scategories",require('./routes/scategorie.route'))
 app.use("/api/articles",require('./routes/article.route'))
 app.use("/api/users",require('./routes/user.route'))
-
+//dist reactjs 
+app.use(express.static(path.join(__dirname, './client/build'))); // Route pour les pages non trouvées, redirige vers index.html  
+app.get('*', (req, res) => { res.sendFile(path.join(__dirname,'./client/build/index.html')); }); 
 
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}`);
